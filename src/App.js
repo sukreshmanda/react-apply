1fed25651758074a21b1ef2526b09bf085a8676f
sukreshmanda <mandasukresh@gmail.com>
2022-10-07 10:52:42 +0530

    Lists and keys

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
+import { useState } from 'react';
 import './App.css';
 import ClassComponent from './libs/components/ClassComponent';
 import Functional from './libs/components/Functional';
 import ConditionalRendering from './libs/conditional-rendering/ConditionalRendering';
 import JSXExample from './libs/JSX/JSX';
+import Welcome from './libs/lifecycle/Welcome';
+import ListsAndKeys from './libs/lists&keys/ListsAndKeys';
 import Props from './libs/props-state/Props';
 import State from './libs/props-state/State';
 
 function App() {
-  const name = "sukresh";
   return (
     <div className="App">
-      <ConditionalRendering/>
+      <ListsAndKeys/>
     </div>
   );
 }
